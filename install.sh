7f484d00b7ad7446098aa8d4bc27b9f940995bb3
Dhruv Vyas <Dastan@Dhruvs-MacBook-Pro.local>
2015-08-15 15:58:29 +0530

    First commit

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo 'Fetching ns2 source'
+wget -O ns-allinone-2.35.tar.gz http://sourceforge.net/projects/nsnam/files/allinone/ns-allinone-2.35/ns-allinone-2.35.tar.gz/download
+echo 'Source downlaoded.'
+tar xvzf ns-allinone-2.35.tar.gz
+cd ns-allinone-2.35
+echo 'Downloading patch files.'
+wget https://raw.githubusercontent.com/dhruvvyas90/ns-allinone-2.35-Mac-OS-10.9-install-patch/master/install64
+chmod +x install64
+wget https://raw.githubusercontent.com/dhruvvyas90/ns-allinone-2.35-Mac-OS-10.9-install-patch/master/ns_common_tclAppInit.cc.patch
+wget https://raw.githubusercontent.com/dhruvvyas90/ns-allinone-2.35-Mac-OS-10.9-install-patch/master/ns_common_tkAppInit.cc.patch
+wget https://raw.githubusercontent.com/dhruvvyas90/ns-allinone-2.35-Mac-OS-10.9-install-patch/master/ns_linkstate_ls.h.patch
+wget https://raw.githubusercontent.com/dhruvvyas90/ns-allinone-2.35-Mac-OS-10.9-install-patch/master/ns_makefile.patch
+echo "Patch downloaded"
+echo "Installing..."
+./install64
+echo "Installation complete"
+echo "Installing paths"
+echo "export PATH=$PATH:`pwd`/ns-2.35:`pwd`/nam-1.15" >> ~/.bash_profile
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/otcl-1.14:`pwd`/lib" >> ~/.bash_profile
+echo "export TCL_LIBRARY=$TCL_LIBRARY:`pwd`/tcl8.5.10/" >> ~/.bash_profile
+source ~/.bash_profile
+echo 'Path installation complete. Run ns or nam'
+echo 'cleaning up...'
+cd ..
+rm ns-allinone-2.35.tar.gz
+echo 'Installation complete'
